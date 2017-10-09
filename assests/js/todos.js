9b2ae60f3e0b647e008ead3b220e57316291346f
John Erickson <ericksonjohn08@hotmail.com>
2017-10-09 19:29:41 -0400

    Add eventListener to span

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,3 +1,12 @@
 $('li').click(function() {
 	$(this).toggleClass('completed');
 });
+
+$('span').click(function(event) {
+	$(this).parent().fadeOut('slow', function() {
+		$(this).remove();
+	});
+	
+	// this will stop event bubbling; after clicking a span it will not fire the click event on the li
+	event.stopPropagation();
+});
