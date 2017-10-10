e07972342ee0128494348d7fdcbd516e725e2140
John Erickson <ericksonjohn08@hotmail.com>
2017-10-10 09:13:41 -0400

    Add font awesome directory. Add plus and garbage icon. Create span css

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -20,6 +20,6 @@ $('input[type="text"]').keypress(function(event) {
 		var toDoText = $(this).val();
 		
 		$(this).val("");
-		$('ul').append('<li><span>X</span> ' + toDoText + '</li>');
+		$('ul').append('<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> ' + toDoText + '</li>');
 	}
 });
